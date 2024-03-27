7ef7708679ca3d42bd7911d4beb2b3b6fdfa14eb
Rodrick <other@deadinsideartist.art>
2024-03-27 17:12:33 -0500

    Functional

--- functions.php
+++ functions.php
@@ -18,12 +18,35 @@ function importCSV($filepath) {
 }
 
 function showButtons($buttons) {
-    echo "<section>";
+    echo '<form enctype="multipart/form-data" action="/" method="post">';
     foreach ($buttons as $button) {
-        echo '<button type="button">'.$button["name"].": ".$button["number"]."</button>";
+        echo '<button type="submit" name="'.$button["id"].'" value="'.$button["name"].'">'.$button["name"].": ".$button["number"].'</button>';
     }
-    echo "</section>";
+    echo "</form>";
 }
 
+function updateCount($button, $filepath) {
+    $temp_file = fopen("temp.csv", "w");
+    $file = fopen($filepath, "r");
+    $buttons = importCSV($filepath);
+
+    fputcsv($temp_file, array_keys($buttons[0]));
+    try {
+        foreach ($buttons as $b) {
+            if ($button["id"] == $b["id"]) {
+                $button["number"] += 1;
+                fputcsv($temp_file, $button);
+            } else {
+                fputcsv($temp_file, $b);
+            }
+        }
+    } catch (Exception $error) {
+        echo $error->getMessage();
+    }
+
+    fclose($temp_file);
+    fclose($file);
+    rename("temp.csv", $filepath);
+}
 
 ?>
--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
     include "functions.php";
-    
+
+    foreach (importCSV("buttons.csv") as $button) {
+        if (isset($_POST[$button["id"]])) {
+            updateCount($button, "buttons.csv");
+        }
+    }
+
     showButtons(importCSV("buttons.csv"));
 ?>
-
-<script>
-</script>
