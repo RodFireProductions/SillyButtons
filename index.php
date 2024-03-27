d41d54f104ca9ef6766f2e7b0a94c97c26a8eb67
Rodrick <other@deadinsideartist.art>
2024-03-27 18:58:20 -0500

    Less files plus styling

--- index.php
+++ index.php
@@ -1,11 +1,130 @@
-<?php
-    include "functions.php";
+<!doctype html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<style>
+			:root {
+				--button-color			: #5065FF;
+				--button-hover-color	: #3244c2;
+				--font-color			: white;
+				--background-color		: black;
+				
+				--font-size: calc(1rem);
+			}
+			
+			html { height: 100% }
+			body {
+				margin: 0;
+				background-color: var(--background-color);
+				height: 100%;
+			}
+			
+			main {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				
+				height: 100%;
+				width: 100%;
+			}
+			
+			button {
+				background-color: var(--button-color);
+				border: none;
+				border-radius: 50px;
+				padding: .5em 1em;
+				margin-bottom: .3em;
+				
+				font-weight: 700;
+				color: var(--font-color);
+				font-size: var(--font-size);
+				
+				float: right;
+				
+				cursor: pointer;
+			}
+			
+			button:hover {
+				background-color: var(--button-hover-color);
+			}
+			
+			button span {
+				background-color: var(--font-color);
+				color: var(--button-color);
+				
+				padding: 0.1em .25em 0.1em .25em;
+				border-radius: 5px;
+			}
+		</style>
+	</head>
+	<body>
+		<main>
+			<?php
+			// -- Functions -- //
 
-    foreach (importCSV("buttons.csv") as $button) {
-        if (isset($_POST[$button["id"]])) {
-            updateCount($button, "buttons.csv");
-        }
-    }
+			function importCSV($filepath) {
+				$button = [];
+				$csv = fopen($filepath, "r");
+				while ($values = fgetcsv($csv)) {
+					$button[] = $values;
+				}
+				fclose($csv);
 
-    showButtons(importCSV("buttons.csv"));
-?>
+				$keys = array_shift($button);
+				$buttons = [];
+				foreach ($button as $values) {
+					$buttons[] = array_combine($keys, $values);
+				}
+
+				return $buttons;
+			}
+
+			function showButtons($buttons) {
+				echo '<form enctype="multipart/form-data" action="/" method="post">';
+				foreach ($buttons as $button) {
+					echo '<button type="submit" name="'.$button["id"].'" value="'.$button["name"].'">'.$button["name"].' <span>'.$button["number"].'</span></button><br>';
+				}
+				echo "</form>";
+			}
+
+			function updateCount($button, $filepath) {
+				$temp_file = fopen("temp.csv", "w");
+				$file = fopen($filepath, "r");
+				$buttons = importCSV($filepath);
+
+				fputcsv($temp_file, array_keys($buttons[0]));
+				try {
+					foreach ($buttons as $b) {
+						if ($button["id"] == $b["id"]) {
+							$button["number"] += 1;
+							fputcsv($temp_file, $button);
+						} else {
+							fputcsv($temp_file, $b);
+						}
+					}
+				} catch (Exception $error) {
+					echo $error->getMessage();
+				}
+
+				fclose($temp_file);
+				fclose($file);
+				rename("temp.csv", $filepath);
+			}
+
+			?>
+
+			<?php
+			// -- Buttons -- //
+			
+				foreach (importCSV("buttons.csv") as $button) {
+					if (isset($_POST[$button["id"]])) {
+						updateCount($button, "buttons.csv");
+					}
+				}
+
+				showButtons(importCSV("buttons.csv"));
+			?>
+		</main>
+	</body>
+</html>
--- readme.md
+++ readme.md
@@ -1,5 +1,16 @@
 # Silly Buttons
+Silly buttons is a PHP button clicker embed.
 
-Silly buttons was inspired by this tumblr post.
+## Set Up
+Requirements: A PHP web server
+
+1. Download files. You only need `index.php` and `buttons.csv`.
+2. Edit `buttons.csv` to the button options you want. Make sure each has its own ID number.
+3. Edit the button styling in `index.php` if you want. There's CSS variables for quick color edits.
+4. Upload your file to your server and CHMOD `buttons.csv` to 640 so it can't be accessed publically.
+5. Embed your buttons on your site's 'about me' page.
+
+## Inspiration
+This project was inspired by this Tumblr post:
 
 [![](inspiration.jpg)](https://tmblr.co/Z6dEMxefHfyaqe00)
